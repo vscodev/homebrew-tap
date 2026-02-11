de4cd655f96bea3f5a35e7a5eb33ebeb6bc419ed
vscodev <71597201+vscodev@users.noreply.github.com>
2026-02-11 23:24:09 +0800

    Release Neokikoeru v1.1.0

--- Formula/neokikoeru.rb
+++ Formula/neokikoeru.rb
@@ -1,23 +1,23 @@
 class Neokikoeru < Formula
   desc "Cloud storage-based web media player for DLsite voice works"
   homepage "https://github.com/vscodev/neokikoeru"
-  version "1.0.1"
+  version "1.1.0"
 
   if OS.mac?
     if Hardware::CPU.arm? || Hardware::CPU.in_rosetta2?
-      url "https://github.com/vscodev/neokikoeru/releases/download/v1.0.1/neokikoeru-macos-arm64.tar.gz"
-      sha256 "a0e1a5fadea7bc50f0121b2cf5a85324fb44b346e55cd4e7a2422e947a73cc9c"
+      url "https://github.com/vscodev/neokikoeru/releases/download/v1.1.0/neokikoeru-macos-arm64.tar.gz"
+      sha256 "fc3830af22e99227fac94f2c3cc12f7a50ebd1c752f9d15a9f4243f2feb91465"
     else
-      url "https://github.com/vscodev/neokikoeru/releases/download/v1.0.1/neokikoeru-macos-amd64.tar.gz"
-      sha256 "d968412e824fd995593d8fbd0d76fe90e8bbc07dfb90ecae4bced789c0ddbb07"
+      url "https://github.com/vscodev/neokikoeru/releases/download/v1.1.0/neokikoeru-macos-amd64.tar.gz"
+      sha256 "caafc42a896ddcdd70cddebfb221878b1759fce0e61cb6fa21fbe41f165c43b7"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/vscodev/neokikoeru/releases/download/v1.0.1/neokikoeru-linux-arm64.tar.gz"
-      sha256 "ac1fbee6795237908379a419630a61994c31ccfdb7f56cd3e703a8462e987a71"
+      url "https://github.com/vscodev/neokikoeru/releases/download/v1.1.0/neokikoeru-linux-arm64.tar.gz"
+      sha256 "b4171432e82a9a8cf71e16717cde269e98725e63157b2b2e80080e2d1964a914"
     else
-      url "https://github.com/vscodev/neokikoeru/releases/download/v1.0.1/neokikoeru-linux-amd64.tar.gz"
-      sha256 "e11aa7776f0f7c366fb556b701bcaf1c3dcfb5b89c24bc6dd43b3cb5a1be550f"
+      url "https://github.com/vscodev/neokikoeru/releases/download/v1.1.0/neokikoeru-linux-amd64.tar.gz"
+      sha256 "15709339cdc712bd2678fa788cbe95299d89e79a94dcf84424a9881fac8c503e"
     end
   else
     odie "Unsupported platform. Please submit a bug report here: https://github.com/vscodev/neokikoeru/issues\n#{OS.report}"
